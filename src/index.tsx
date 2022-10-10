de6487187203032187907637f445f3009382e4e3
Dante <dcoira0119@gmail.com>
2022-10-10 02:56:29 -0400

    redo it all skid someone else yas

--- src/index.tsx
+++ src/index.tsx
@@ -1,98 +1,36 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React, Toasts, Constants } from 'enmity/metro/common';
-import { getByProps, bulk, filters} from 'enmity/metro';
-import { getIDByName } from 'enmity/api/assets';
-import { FormRow } from 'enmity/components';
+import { findInReactTree } from 'enmity/utilities';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
 
-import Settings from './components/Settings';
+import { View } from 'enmity/components';
 
-const [
-   Clipboard,
-   LazyActionSheet,
-   MessageStore,
-   Permissions,
-   User
-] = bulk(
-   filters.byProps('setString'),
-   filters.byProps("openLazy", "hideActionSheet"),
-   filters.byProps("getMessage", "getMessages"),
-   filters.byProps("getChannelPermissions"),
-   filters.byProps('getCurrentUser')
-);
+const Patcher = create('dashless');
 
-const Patcher = create('silent-typing');
+const dashRegExp = new RegExp('-', 'g');
 
-const SilentTyping: Plugin = {
+const Dashless: Plugin = {
    ...manifest,
 
    onStart() {
-      let DeleteMessage = getByProps("deleteMessage")
-         Patcher.before(LazyActionSheet, "openLazy", (_, [component, sheet], _res) => {
-            if (sheet === "MessageLongPressActionSheet") {
-               component.then((instance) => {
-                  Patcher.after(instance, "default", (_, message, res) => {
-                     if (!res.props) {
-                        console.log(`[Cut Local Error: Property "Props" Does not Exist on "res"]`)
-                        return res;
-                     }
-                     let finalLocation = res?.props?.children?.props?.children?.props?.children[1]
-                     const addItem = (finalLocation: any) => {
-                        let findItem: any = finalLocation.find((e: any) => e.props.message=="Copy Text")
-                        if (!findItem) return false
-                        let indexOfButon = finalLocation.indexOf(findItem)
-                        return indexOfButon+1
-                     }
-                     if(finalLocation[addItem(finalLocation)].key=='512') { return }
-                     const originalMessage = MessageStore.getMessage(
-                        message[0].message.channel_id,
-                        message[0].message.id
-                        );
-                        const formElem = <FormRow
-                        key="512"
-                        leading = {<FormRow.Icon source={getIDByName("leaf")}/>}
-                        label="Cut Message"
-                        onPress={() => {
-                           Toasts.open({
-                              content: "Cut Message Successfully",
-                              source: getIDByName("ic_text_channel_16px"),
-                        });
-                           // Clipboard.setString(originalMessage.content)
-   
-                           //DeleteMessage.deleteMessage(`${originalMessage.channel_id}`, `${originalMessage.id}`)
-   
-                           LazyActionSheet.hideActionSheet()
-                        }}
-                        
-                        />
-   
-                        const logErr = () => {console.log("Failed to find the 'Copy Text' property, meaning this is likely an embed, or an attachment with no context.")}
-   
-                        let user = User.getCurrentUser()
-                        addItem(finalLocation) ?
-                           originalMessage.author.username===user.username ?
-                              finalLocation.splice(addItem(finalLocation), 0, formElem) 
-                           : Permissions.can(Constants.Permissions.MANAGE_MESSAGES) 
-                              ? finalLocation.splice(addItem(finalLocation), 0, formElem) 
-                           : logErr()
-                        : logErr()
-                  });
-               });
-            }
-         })
-      },
+      const unpatchView = Patcher.after(View, 'render', (_ctx, _args, res) => {
+         const textChannel: any = findInReactTree(res, r => r?.props?.channel?.name && r?.props?.hasOwnProperty?.('isRulesChannel'));
+         if (!textChannel) return;
 
-   
+         Patcher.after(textChannel.type, 'type', (_ctx, _args, res) => {
+            const textChannelName: any = findInReactTree(res, r => typeof r?.children === 'string');
+            if (!textChannelName) return;
 
+            textChannelName.children = textChannelName.children.replace(dashRegExp, ' ');
+            return res;
+         });
+         unpatchView();
+      });
+   },
 
    onStop() {
       Patcher.unpatchAll();
-   },
-
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
    }
 };
 
-registerPlugin(SilentTyping);
+registerPlugin(Dashless);
